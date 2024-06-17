e2d720d52b9a1bace7dac0a8aa63d2eded172480
j0giwa <77389555+j0giwa@users.noreply.github.com>
2024-06-17 19:59:23 +0200

    Container

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,15 @@
 function App() {
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">
+	Klimaralley
+      </h1>
+      <ul>
+	  <li><a href="/play/flut">Die Flut Kommt</a></li>
+	  <li><a href="/play/recycling">Räumen Recyclen Rümplen</a></li>
+	  <li><a href="/play/wasserarm">Wasserarm Satt</a></li>
+      </ul>
+    </>
   )
 }
 
