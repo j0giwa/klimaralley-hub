a078718953e686d4941a51118997bfb20efee848
j0giwa <77389555+j0giwa@users.noreply.github.com>
2024-07-01 12:25:09 +0200

    Theme switcher

--- src/routes/games/Wasserarm.jsx
+++ src/routes/games/Wasserarm.jsx
@@ -23,7 +23,8 @@ function Wasserarm() {
               <p>
                 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
               </p>
-              <div tabindex="0" class="collapse collapse-open collapse-arrow">
+              <div class="collapse collapse-arrow">
+                <input type="checkbox" checked/>
                 <summary class="collapse-title text-xl font-medium">Spielprinzip</summary>
                 <div class="collapse-content">
                   <p>
