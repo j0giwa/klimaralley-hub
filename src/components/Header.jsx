64d3c702cae5ce13473a8f6cf63c62b22da46006
j0giwa <77389555+j0giwa@users.noreply.github.com>
2024-08-16 13:42:59 +0200

    Fallback navlinks for mobile

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -29,38 +29,45 @@ function Header() {
   };
 
   return (
-    <div className="navbar bg-base-100 mb-7 border-solid border-b-2 border-base-200">
-      <div className="navbar-start">
-        <div className="flex-1">
-          <a className="btn btn-ghost text-xl md:text-3xl text-primary" href="/">Klimarallye</a>
+    <div className="mb-7 border-solid border-b-2 border-base-200">
+      <div className="navbar bg-base-100">
+        <div className="navbar-start">
+          <div className="flex-1">
+            <a className="btn btn-ghost text-xl md:text-3xl text-primary" href="/">Klimarallye</a>
+          </div>
+        </div>
+        <div className="navbar-center">
+          <ul className="menu menu-horizontal px-1">
+            <li><a className="hidden lg:block" href="/games/flut">Die Flut Kommt</a></li>
+            <li><a className="hidden lg:block" href="/games/recycling">Räumen Recyclen Rümplen</a></li>
+            <li><a className="hidden lg:block" href="/games/wasserarm">Wasserarm Satt</a></li>
+          </ul>
+        </div>
+        <div className="navbar-end">
+          <ul className="menu menu-horizontal px-1">
+            <li>
+            <details>
+              <summary>Account</summary>
+              <ul className="bg-base-100 rounded-t-none p-2 z-50">
+                <li>
+                  {token ? (
+                    <button onClick={logout}>Logout</button>
+                  ) : (
+                    <a href="/auth/login/">Login</a>
+                  )}
+                </li>
+                <li><a href="/auth/register">Register</a></li>
+              </ul>
+            </details>
+            </li>
+          </ul>
+          <ThemeController />
         </div>
       </div>
-      <div className="navbar-center">
-        <ul className="menu menu-horizontal px-1">
-          <li><a className="hidden lg:block" href="/games/flut">Die Flut Kommt</a></li>
-          <li><a className="hidden lg:block" href="/games/recycling">Räumen Recyclen Rümplen</a></li>
-          <li><a className="hidden lg:block" href="/games/wasserarm">Wasserarm Satt</a></li>
-          </ul>
-      </div>
-      <div className="navbar-end">
-        <ul className="menu menu-horizontal px-1">
-          <li>
-          <details>
-            <summary>Account</summary>
-            <ul className="bg-base-100 rounded-t-none p-2 z-50">
-              <li>
-                {token ? (
-                  <button onClick={logout}>Logout</button>
-                ) : (
-                  <a href="/auth/login/">Login</a>
-                )}
-              </li>
-              <li><a href="/auth/register">Register</a></li>
-            </ul>
-          </details>
-          </li>
-        </ul>
-        <ThemeController />
+      <div class="flex flex-nowrap justify-between px-10 pb-1 lg:hidden">
+          <a href="/games/flut">Flut</a>
+          <a href="/games/recycling">Recyclen</a>
+          <a href="/games/wasserarm">Wasserarm</a>
       </div>
     </div>
   );
